a3aae5532ace769ce82938b75a7a5a786c121e11
Afraz Butt <afraz.jed.grw@gmail.com>
2024-09-27 17:28:40 +0500

    initialize cargo based setting

---
+++ compiler/src/main.rs
@@ -0,0 +1,9 @@
+pub mod lexer;
+
+fn main() {
+    // let source_code = "hindsa x = 10; asharia y = 20.5; agar (x == y) { niklo; } irshaad "10"; wapsi 0;".to_string();
+    // let mut lexer = lexer::LexicalAnalyzer::new(source_code.clone());
+    // let tokens = lexer.tokenize();
+    // println!("\n\n\nSource Code: {} \n\n\n", source_code);
+    // lexer::print_tokens(&tokens);
+}
